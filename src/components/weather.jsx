984381e8ded68d3a8c8a695d8a7195c1f6366759
Felix <34609664+z-felixhan@users.noreply.github.com>
2021-10-29 17:39:30 -0400

    Add placeholders

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -10,9 +10,23 @@ const Weather = () => {
                 <h5>
                     <FontAwesomeIcon icon={ faSun } />
                 </h5>
+                <h1>10&deg;</h1>
+                { minmaxTemp(24, 19) }
+                <h4>Sunny</h4>
             </div>
         </div>
     );
 };
+
+
+const minmaxTemp = (min, max) => {
+    return (
+        <h3>
+            <span>{ min }&deg;</span>
+            <span>{ max }&deg;</span>
+        </h3>
+    );
+};
+
 
 export default Weather;
