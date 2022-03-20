662ff5213f7e26b4dc5e052e8c8fc6f6b20596fe
Felix <34609664+z-felixhan@users.noreply.github.com>
2022-03-20 02:48:42 -0400

    Update styling

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -5,36 +5,38 @@ import { CircularProgress } from "@mui/material";
 const Weather = (props) => {
   return (
     <div className="weather-container">
-      {!props.loading ? (
-        <div>
-          <h1 className="location">{props.location}</h1>
-          <h5 className="weather-icon">
-            <img
-              src={
-                props.icon &&
-                "http://openweathermap.org/img/wn/" + props.icon + "@2x.png"
-              }
-              alt=""
+      <div className="weather-sub-container">
+        {!props.loading ? (
+          <div>
+            <h1 className="location">{props.location}</h1>
+            <h5 className="weather-icon">
+              <img
+                src={
+                  props.icon &&
+                  "http://openweathermap.org/img/wn/" + props.icon + "@2x.png"
+                }
+                alt=""
+              />
+            </h5>
+            <h4 className="description">{props.description}</h4>
+            <h1>
+              <span className="temp" onClick={props.setCount}>
+                {props.temp}
+              </span>
+            </h1>
+            {minmaxTemp(props.temp_min, props.temp_max, props)}
+          </div>
+        ) : (
+          <div className="loader-container">
+            <CircularProgress
+              sx={{
+                position: "relative",
+                top: "50%",
+              }}
             />
-          </h5>
-          <h4 className="description">{props.description}</h4>
-          <h1>
-            <span className="temp" onClick={props.setCount}>
-              {props.temp}
-            </span>
-          </h1>
-          {minmaxTemp(props.temp_min, props.temp_max, props)}
-        </div>
-      ) : (
-        <div className="loader-container">
-          <CircularProgress
-            sx={{
-              position: "relative",
-              top: "50%",
-            }}
-          />
-        </div>
-      )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
